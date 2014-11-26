1924766327c727f55adddd3f93fa4f0d14490331
creative dev <exam@gmail.com>
2014-11-27 01:44:07 +0800

    After unit test.

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+$dataDir = __DIR__."/../data";
+$CONFIG = array(
+"passwordsalt" => '83768650467763356004851182248962',
+"datadirectory" => $dataDir,//'/var/www/owncloud/data',
+"dbtype" => 'sqlite3',
+"version" => '4.0.6',
+"installed" => true,
+"maxZipInputSize" => '133169152',
+"allowZipDownload" => true,
+"loglevel" => '0',
+"ldapIgnoreNamingRules" => false,
+"versions" => 'false',
+);
+?>
